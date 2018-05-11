d0d1a5b2c8999b012ce4e147010950600632e386
tango-art <tango1991@gmail.com>
2018-05-11 13:37:43 +0300

    Home work lesson 3

---
+++ lesson3/artem.sharpilo.3.4/SimpleCalc/SimpleCalc.java
@@ -0,0 +1,25 @@
+package SimpleCalc;
+
+public class SimpleCalc {
+    public static void main (String[] args ){
+
+        int first= 15;
+        int second =12;
+        String plus = "addition";
+        String minus = "subtraction";
+        String multiply = "multiplication";
+        String divide = "division";
+        String remaind= "remainder";
+        String mod = "modulus";
+
+        System.out.println(plus + " of "+first+" and "+second+ " results in: "+ (first+second));
+        System.out.println(minus + " of "+first+" and "+second+ " results in: "+ (first-second));
+        System.out.println(multiply + " of "+first+" and "+second+ " results in: "+ (first*second));
+        System.out.println(divide + " of "+first+" and "+second+ " results in: "+ ((double)first/second));
+        System.out.println(remaind + " of "+first+" and "+second+ " results in: "+ (first%second));
+        System.out.println(mod + " of "+first+" and "+second+ " results in: "+ (Math.abs(first))+" and "+ Math.abs(second));
+
+
+
+    }
+}
