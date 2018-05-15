9768ec48a68c19b02c7a9a71c5c97f75f496265a
AndrewChern <nrnk3373@gmail.com>
2018-05-15 15:43:41 +0300

    Changeg package name

---
+++ andrew.chern/lesson3/appartmentsCostCalculator.java
@@ -0,0 +1,17 @@
+public class appartmentsCostCalculator {
+    public void counter (int days, int cost, int discountMoreFiveDays, int discountMoreSevenDays){
+
+        int totalSum;
+
+        totalSum = days*cost;
+
+        if (days>5&&days<7){
+            totalSum=totalSum-discountMoreFiveDays;
+        }
+
+        if (days>7){
+            totalSum=totalSum-discountMoreSevenDays;
+        }
+        System.out.println("Appartments cost for " + days + " is " + totalSum);
+    }
+}
