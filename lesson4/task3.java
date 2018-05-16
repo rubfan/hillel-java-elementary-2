06c90970b98e558da6f44689229a515fc9e1d7b5
Sergey Levkovsky <39032131+blessedSir@users.noreply.github.com>
2018-05-16 11:32:33 +0300

    Delete task3.java

--- lesson4/task3.java
+++
@@ -1,9 +0,0 @@
-package lesson4;
-
-public class task3 {
-    public static void main(String[] args) {
-        for (int a = 90; a>=0;a-=5  ){
-            System.out.println( a + " ");
-        }
-    }
-}
