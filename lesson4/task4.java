bc480ec58f24e0eaad609dc881d5eecc2f2717de
Sergey Levkovsky <39032131+blessedSir@users.noreply.github.com>
2018-05-16 11:32:39 +0300

    Delete task4.java

--- lesson4/task4.java
+++
@@ -1,9 +0,0 @@
-package lesson4;
-
-public class task4 {
-    public static void main(String[] args) {
-        for(int a = 0,b = 2;a<=20; a++,b*=2 ){
-            System.out.println(b+ " ");
-        }
-    }
-}
