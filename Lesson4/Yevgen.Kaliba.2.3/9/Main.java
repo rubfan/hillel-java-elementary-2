d2724d2671101d8c6e630e3d3410a5acef329709
Evgen Kaliba <evkal22@gmail.com>
2018-05-21 19:02:04 +0300

    Добавил домашнее задание к 5 уроку

--- Lesson4/Yevgen.Kaliba.2.3/9/Main.java
+++ Lesson4/Yevgen.Kaliba.2.3/9/Main.java
@@ -1,10 +1,11 @@
+package com.company;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
             Scanner MyKeyb = new Scanner(System.in);
             System.out.println("Input number to convert:");
-            int numToConv = MyKeyb.nextInt();
+            int numToConv = MyKeyb.nextInt(),b;
             StringBuffer MyS = new StringBuffer();
              while (numToConv >0) {
                  if (numToConv%2==0) {
@@ -17,5 +18,5 @@ public class Main {
              }
         MyS.reverse();
         System.out.println("Your number in binary form is: "+MyS);
-    }
+        }
 }
