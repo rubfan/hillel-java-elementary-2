cd3b3d0b49d279114de0e1ae4c8d3ad101089038
Yevgen Kaliba <evkal22@gmail.com>
2018-05-28 17:35:29 +0300

    Переименовал папки для соответсвия стандарту

--- Lesson4/Yevgen.Kaliba.2.3/1/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        int x;
-        for (x = 1000; x < 9999; x = x + 3) {
-            System.out.println(x);
-        }
-    }
-}
--- Lesson4/Yevgen.Kaliba.2.3/3/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        int x3,y3;
-        for (x3=90; x3>=0; x3=x3-5) {
-            System.out.println(x3);
-        }
-    }
-}
