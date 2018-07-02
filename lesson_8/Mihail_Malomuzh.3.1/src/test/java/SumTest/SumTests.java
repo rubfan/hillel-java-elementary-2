11e2f7006cba497ee408bbcdec111a1ffa4c6491
Mihail Malomuzh <mihail.malomuzh@gmail.com>
2018-07-02 17:01:24 +0300

    test bug fix

--- lesson_8/Mihail_Malomuzh.3.1/src/test/java/SumTest/SumTests.java
+++ lesson_8/Mihail_Malomuzh.3.1/src/test/java/SumTest/SumTests.java
@@ -1,7 +1,5 @@
 package SumTest;
 
-import static ArraysForTests.ArraysFromHomeWork6.Merge;
-
 import static ArraysForTests.ArraysFromHomeWork6.Sum;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -29,17 +27,14 @@ public class SumTests {
     @Test
     public void testSumMustTrue(){
         for (int i = 1; i < 10000; i++) {
-            ind2 = rnd.nextInt(a.length - 1);
-            ind1 = rnd.nextInt(a.length - 1);
+            ind2 = rnd.nextInt(a.length - 2) + 1;
+            ind1 = rnd.nextInt(ind2);
             assertThat(Sum(a, a[ind1] + a[ind2]), is(true));
         }
     }
     @Test
     public void testSumMustFalse(){
-         for (int i = 1; i < 10000; i++) {
-             assertThat(Sum(a, 2* a[a.length - 1] + 1), is(false));
-        }
+        assertThat(Sum(a, 2* a[a.length - 1] + 1), is(false));
     }
-
 
 }
