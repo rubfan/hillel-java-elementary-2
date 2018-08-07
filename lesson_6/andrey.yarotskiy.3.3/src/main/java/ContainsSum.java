d68b27156ee88b23cb27f67870823794edd6641a
Andrey <yrtsky@gmail.com>
2018-08-07 16:32:31 +0300

    Added tests

--- lesson_6/andrey.yarotskiy.3.3/src/main/java/ContainsSum.java
+++ lesson_6/andrey.yarotskiy.3.3/src/main/java/ContainsSum.java
@@ -4,16 +4,21 @@ public class ContainsSum {
         int a[] = {1, 5, 7, 9, 12};
         int sum = 12;
 
+        containsSum(a,sum);
+        System.out.println("False");
+    }
+
+    public static boolean containsSum(int[] a, int sum){
         for (int i = 0; i < a.length; i++) {
             for (int j = i+1; j < a.length && a[i] + a[j] <=sum; j++) {
                 if(a[i] + a[j] == sum){
                     System.out.println("True: " + a[i] + " + " + a[j] + " = " + sum);
-                    return;
+                    return true;
                 }
 
             }
-            
+
         }
-        System.out.println("False");
+        return false;
     }
 }
--- lesson_6/andrey.yarotskiy.3.3/src/main/java/MergeArrays.java
+++ lesson_6/andrey.yarotskiy.3.3/src/main/java/MergeArrays.java
@@ -5,7 +5,16 @@ public class MergeArrays {
     public static void main(String[] args) {
         int[] a = {1, 3, 5};
         int[] b = {2, 4, 8, 9, 12};
-        int[] c = new int[a.length+b.length];
+
+        int[] c = merge(a,b);
+
+        for (int i = 0; i < c.length; i++) {
+            System.out.print(c[i] + " ");
+        }
+    }
+
+    public static int[] merge(int[] a, int[] b){
+        int[] merged = new int[a.length+b.length];
 
         if(b.length < a.length){
             int temp[] = a;
@@ -13,7 +22,7 @@ public class MergeArrays {
             b = temp;
         }
 
-        for (int i = 0; i < c.length; i++) {
+        for (int i = 0; i < merged.length; i++) {
             int min;
             if(a.length != 0 && a[0] < b[0]){
                 min = a[0];
@@ -22,11 +31,8 @@ public class MergeArrays {
                 min = b[0];
                 b = Arrays.copyOfRange(b,1,b.length);
             }
-            c[i] = min;
+            merged[i] = min;
         }
-
-        for (int i = 0; i < c.length; i++) {
-            System.out.print(c[i] + " ");
-        }
+        return merged;
     }
 }
